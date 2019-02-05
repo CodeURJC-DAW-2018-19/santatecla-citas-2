0adc4a0a562250c34e50539b4574c2eb2b2bd931
Christian Taidi <christiantaidi@gmail.com>
2019-02-05 16:43:46 +0100

    added theme, text and quote controller implementations all without functionality,

---
+++ src/main/java/com/urjc/daw/practica/controller/impl/QuoteControllerImpl.java
@@ -0,0 +1,40 @@
+package com.urjc.daw.practica.controller.impl;
+
+import com.urjc.daw.practica.controller.QuoteController;
+import com.urjc.daw.practica.model.Quote;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+public class QuoteControllerImpl implements QuoteController {
+    @Override
+    @RequestMapping(value = "/quote/@id",method = RequestMethod.GET)
+    public Quote getQuote() {
+        return null;
+    }
+
+    @Override
+    @RequestMapping(value = "/quote",method = RequestMethod.GET)
+    public List<Quote> getAllQuotes() {
+        return null;
+    }
+
+    @Override
+    @RequestMapping(value = "/quote/@id",method = RequestMethod.POST)
+    public boolean postQuote(Quote quote) {
+        return false;
+    }
+
+    @Override
+    @RequestMapping(value = "/quote/@id",method = RequestMethod.PUT)
+    public boolean editQuote(Quote quote) {
+        return false;
+    }
+
+    @Override
+    @RequestMapping(value = "/quote/@id",method = RequestMethod.DELETE)
+    public boolean deleteQuote(Quote quote) {
+        return false;
+    }
+}
