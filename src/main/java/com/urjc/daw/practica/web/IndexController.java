850fb47578e5d3d0f40e46adfbdf519277b03634
Yoshiki <yoshiki.r@gmail.com>
2019-02-22 16:15:14 +0100

    added quote mapping

--- src/main/java/com/urjc/daw/practica/web/IndexController.java
+++ src/main/java/com/urjc/daw/practica/web/IndexController.java
@@ -1,14 +1,18 @@
 package com.urjc.daw.practica.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.urjc.daw.practica.model.Quote;
 import com.urjc.daw.practica.service.QuoteManagementService;
 
 @RestController
@@ -28,6 +32,17 @@ public class IndexController{
 		System.out.println(mav.toString());
 		System.out.println(qms.findAll().toString());
 		return mav;
+	}
+	
+	@GetMapping("/quote/{id}")
+	public String showBook(Model model, @PathVariable long id) {
+		
+		Optional<Quote> quote = qms.findOne(id);
+
+		if(quote.isPresent()) {
+			model.addAttribute("book", quote.get());
+		}
+		return "book";
 	}
 	
 	@GetMapping("/error")
