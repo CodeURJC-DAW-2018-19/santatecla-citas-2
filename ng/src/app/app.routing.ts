dad2575385055ada63dfcb810d775513746b7eea
Christian Taidi <christiantaidi@gmail.com>
2019-04-02 16:18:21 +0200

    added routing for topic click

--- ng/src/app/app.routing.ts
+++ ng/src/app/app.routing.ts
@@ -3,13 +3,18 @@ import { RouterModule } from '@angular/router';
 import { BookListComponent } from './book-list.component';
 import { BookDetailComponent } from './book-detail.component';
 import { BookFormComponent } from './book-form.component';
+import { TopicListComponent } from './topic/topic-list.component';
+import {TopicDetailComponent} from './topic/topic-detail.component';
 
 const appRoutes = [
-  { path: 'books', component: BookListComponent, useAsDefault: true },
+  { path: 'books', component: BookListComponent },
+  { path: 'topics', component: TopicListComponent},
+  { path: 'topic/:id', component: TopicDetailComponent },
   { path: 'book/new', component: BookFormComponent },
+
   { path: 'book/:id', component: BookDetailComponent },
   { path: 'book/edit/:id', component: BookFormComponent },
-  { path: '', redirectTo: 'books', pathMatch: 'full' }
+  { path: '', redirectTo: 'topics', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
