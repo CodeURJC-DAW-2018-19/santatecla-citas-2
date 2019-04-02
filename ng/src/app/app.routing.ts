a7883e6e420581912751e8ba840d0f5cafbc19c3
re-beyeme <resty.beyeme@gmail.com>
2019-04-02 12:09:50 +0200

    environments
    Carpeta environments creada

--- ng/src/app/app.routing.ts
+++ ng/src/app/app.routing.ts
@@ -1,20 +1,15 @@
-import { QuoteDetailComponent } from './../quote/quote-detail.component';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import { QuoteListComponent } from './quote-list.component';
-import { QuoteDetailComponent } from './quote-detail.Component';
-import { QuoteFormComponent } from './quote-form.component';
-import { QuoteListComponent } from './quote/quote-list.component';
-import { QuoteFormComponent } from './quote/quote-form.component';
-import { TopicListComponent } from './topic/topic-list.component';
+import { BookListComponent } from './book-list.component';
+import { BookDetailComponent } from './book-detail.component';
+import { BookFormComponent } from './book-form.component';
 
 const appRoutes = [
-  { path: 'quotes', component: QuoteListComponent },
-  { path: 'quotes/new', component: QuoteFormComponent },
-  { path: 'topics', component: TopicListComponent, useAsDefault: true},
+  { path: 'books', component: BookListComponent, useAsDefault: true },
+  { path: 'book/new', component: BookFormComponent },
   { path: 'book/:id', component: BookDetailComponent },
   { path: 'book/edit/:id', component: BookFormComponent },
-  { path: '', redirectTo: 'topics', pathMatch: 'full' }
+  { path: '', redirectTo: 'books', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
