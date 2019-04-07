7b9f513c29b3b173be9b385aa99a332dfbf576a1
Yoshiki <yoshiki.r@gmail.com>
2019-04-07 04:24:31 +0200

    Added charts and spring styled index

--- backend/src/main/java/com/urjc/daw/practica/api/ChartRestController.java
+++ backend/src/main/java/com/urjc/daw/practica/api/ChartRestController.java
@@ -8,18 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.urjc.daw.practica.service.impl.GraphService;
+import com.urjc.daw.practica.service.impl.ChartService;
 
 @RestController
 @RequestMapping("api/chart")
 public class GraphRestController {
 	
 	@Autowired
-	GraphService graphService;
+	ChartService chartService;
 
 	@GetMapping("")
 	public ResponseEntity<HashMap<String,Integer>> getGraph(){
-		HashMap<String,Integer> found = graphService.getGraph();
+		HashMap<String,Integer> found = chartService.getGraph();
 		if(!found.isEmpty()) {
 			return new ResponseEntity<>(found,HttpStatus.OK);
 		}else {
